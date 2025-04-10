4a5a2feee5fb3ed7551d36efee7d3b31e6ddef4a
Bidur poudel <129536448+BidurPoudel@users.noreply.github.com>
2025-04-10 20:25:36 +0545

    calling the private variable

--- basic_practice/oops/inherit.py
+++ basic_practice/oops/inherit.py
@@ -42,3 +42,54 @@ dog = Dog("Jacky", 2, "Pitbull")
 print("dog name is: ", dog.getName)
 print("dog age is: " , dog.getAge)
 print("Dog breed is: ", dog.getBreed)
+
+
+
+
+# understanding to calling of private variable from Parent class to child class
+
+class Person:
+    def __init__(self, name:str, age:int):
+        self.__name:str = name
+        self.__age:int= age
+    
+    def getName(self):
+        print(self.__name)
+        return self.__name
+    
+    def setName(self, name:str):
+        self.__name = name
+
+
+
+class Student(Person):
+    def __init__(self, name, age, section):
+        super().__init__(name, age)
+        self.__section= section
+
+    def studentSection(self):
+        print(f'{self.getName()} is from {self.__section}')
+
+class Teacher(Person):
+        def __init__(self, name, age, subject):
+            super().__init__(name, age)
+            self.__subject= subject
+
+        def teacherSub(self):
+            print(f'{self.getName()} teaches {self.__subject}')
+
+student=Student('bidur', 23, 'B')
+
+student.getName() #student object can call function of class Person
+
+student.studentSection()
+teacher=Teacher('Shiva', 'UNKNOWN', 'Python')
+teacher.teacherSub()
+
+# """when variable is private, if we want to call them from child class we should call the getter method of
+# parent class like
+
+# getName(): > it is from person class
+# calling it in child class like  print(f'{self.getName()} teaches {self.__subject}')
+
+# """
